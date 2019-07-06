bcd1de23ae57c1ec1704b0c8e14a4bcc3774824f
jadwiga wyczesany <jmwyczesany@gmail.com>
2019-07-06 09:39:56 +0200

    initial commit

---
+++ src/main/java/pl/sda/eventorganizer/EventOrganizerApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.eventorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventOrganizerApplication.class, args);
+	}
+
+}
